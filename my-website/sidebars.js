239a49be3b388a37f4bb5930929b96dd42b7183c
wang <ccna8008@163.com>
2020-06-05 18:29:17 +0800

    更新文章-sonar安装

--- my-website/sidebars.js
+++ my-website/sidebars.js
@@ -2,6 +2,7 @@
 module.exports = {
   someSidebar: {
     Apache: ['htaccess','apache-vhost','apache常见问题','apache配置ssl证书','http转https'],
-    Case: ['命令行终端设置代理','CLOSE_WAIT案例','精准定位问题']
+    Case: ['命令行终端设置代理','CLOSE_WAIT案例','精准定位问题'],
+    工具: ['安装sonar']
   },
 };
